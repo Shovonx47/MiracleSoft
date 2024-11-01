16a3e854d3fd5ad2e039f66381279a646626d688
farukphero <farukk6319@gmail.com>
2024-11-01 16:17:30 +0600

    services problem solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Banner from "@/components/Home/Banner";
 import Contact from "@/components/Home/Contact/Contact";
 import Pricing from "@/components/Home/Pricing/Pricing";
-import Services from "@/components/Home/services/Services";
+import Services from "@/components/Home/Service/Service";
+ 
  
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <Services />
+      <Services/>
       <Pricing/>
       <Contact />
     </>
