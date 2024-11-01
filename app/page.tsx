47790f14424397915e5d8d9f5d18e3f9aeeeaec7
farukphero <farukk6319@gmail.com>
2024-11-01 16:13:43 +0600

    services problem solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Banner from "@/components/Home/Banner";
 import Contact from "@/components/Home/Contact/Contact";
 import Pricing from "@/components/Home/Pricing/Pricing";
- 
+import Services from "@/components/Home/Services/Services";
+
+
+
 
 export default function Home() {
   return (
     <>
       <Banner />
-      {/* <Services /> */}
+      <Services />
       <Pricing />
       <Contact />
     </>
--- components/Home/services/services.tsx
+++ components/Home/services/services.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { cards } from "./data";
 import CardComponent from "./Card";
+ 
+ 
 
  
 
